9b304d8d2476c5d7a3ffcceed189e4cffa7b0a47
Ben Schumacher <benschumacher0@gmail.com>
2023-09-20 19:57:43 +0900

    added PyQt for controls

--- torch/img-ae/img-ae.py
+++ torch/img-ae/img-ae.py
@@ -3,6 +3,7 @@
 import torch
 from torch import nn
 
+
 class Autoencoder(nn.Module):
     def __init__(self, layers=3, input_dim=28*28, target_dim=1):
         super(Autoencoder, self).__init__()
@@ -59,9 +60,9 @@ img = img/255.0
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 
-
+TARGET_DIM = 3
 # create model
-model = Autoencoder(layers=5, input_dim=28*28, target_dim=1)
+model = Autoencoder(layers=5, input_dim=28*28, target_dim=TARGET_DIM)
 model.to(device)
 
 # create optimizer
@@ -73,31 +74,30 @@ loss_fn = nn.MSELoss()
 # create data loader
 data_loader = torch.utils.data.DataLoader(img, batch_size=64, shuffle=True)
 
-# train model
-EPOCHS = 20
-model.train()
-for epoch in range(EPOCHS):
-    for data in data_loader:
-        data = data.to(device)
-        output = model(data)
-        loss = loss_fn(output, data)
+TRAIN = True
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+if TRAIN:
+    # train model
+    EPOCHS = 20
+    model.train()
+    for epoch in range(EPOCHS):
+        for data in data_loader:
+            data = data.to(device)
+            output = model(data)
+            loss = loss_fn(output, data)
 
-    print("Epoch [{}/{}], Loss: {:.4f}".format(epoch+1, EPOCHS, loss.item()))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print("Epoch [{}/{}], Loss: {:.4f}".format(epoch+1, EPOCHS, loss.item()))
+else:
+    # load model
+    model.load_state_dict(torch.load("model.pth"))
 
 
 model.eval()
-# generate images
-values = torch.arange(0, 1, 1/64).to(device).view(-1, 1)
-#values = torch.cat((values, values), dim=1)
-output = model.decode(values)
-output = output.view(-1, 28, 28) * 255.0
-output = output.round().to(torch.uint8).cpu().numpy()
 
-import PIL
 from PIL import Image, ImageSequence
 
 #scale images
@@ -107,18 +107,79 @@ FACTOR = 4
 
 
 # save as gif
-images = []
-for i in range(output.shape[0]):
-    img = Image.fromarray(output[i,:,:])
-    images.append(img)
+# images = []
+# for i in range(output.shape[0]):
+#     img = Image.fromarray(output[i,:,:])
+#     images.append(img)
 
-images[0].save('img-ae.gif', save_all=True, append_images=images[1:], duration=100, loop=0)
+# images[0].save('img-ae.gif', save_all=True, append_images=images[1:], duration=100, loop=0)
 
 
 
 # save model
 torch.save(model.state_dict(), "model.pth")
 
+
+# show image
+
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QSlider, QMainWindow
+from PyQt5.QtGui import QPixmap
+
+app = QApplication([])
+
+window = QMainWindow()
+window.resize(800, 800)
+window.setWindowTitle("Digit decoder")
+
+layout = QVBoxLayout()
+widget = QWidget()
+window.setCentralWidget(widget)
+widget.setLayout(layout)
+
+label = QLabel("Hello World")
+layout.addWidget(label)
+
+# show image in qt
+from PIL import ImageQt
+
+values = [0.0 for i in range(TARGET_DIM)]
+def regenerate():
+    t = torch.tensor([values]).to(device)
+    output = model.decode(t)
+    output = output.view(-1, 28, 28) * 255.0
+    output = output.round().to(torch.uint8).cpu().numpy()
+
+    img = Image.fromarray(output[0,:,:])
+    img = img.resize((img.width*FACTOR, img.height*FACTOR))
+    img = ImageQt.ImageQt(img)
+    pixmap = QPixmap.fromImage(img)
+    # scale by 4
+    pixmap = pixmap.scaledToWidth(pixmap.width()*4)
+    label.setPixmap(pixmap)
+
+regenerate()
+
+def sliderCallback(id):
+    def callback(value):
+        value = value/100
+        values[id] = value
+        regenerate()
+    
+    return callback
+
+for i in range(TARGET_DIM):
+    slider = QSlider()
+    slider.valueChanged.connect(sliderCallback(i))
+    slider.setOrientation(1)
+    slider.setMinimum(0)
+    slider.setMaximum(100)
+    slider.setValue(0)
+    layout.addWidget(slider)
+
+
+window.show()
+
+app.exec_()
 
 
         
